6640f337dca14358427968b19c16282ecf9018ca
CherifiMi <cherifi154949@gmail.com>
2022-09-28 18:58:58 +0100

    first vec use

---
+++ src/main/kotlin/Vector.kt
@@ -0,0 +1,25 @@
+import processing.core.PApplet
+import processing.core.PVector
+
+fun main(){
+    Vector
+}
+
+object Vector : PApplet() {
+    init {
+        this.setSize(400, 400)
+        this.runSketch()
+    }
+
+    var pos= PVector(0f, 0f)
+
+    override fun draw() {
+        stroke(0)
+        strokeWeight(2f)
+        fill(125f)
+        ellipse(pos.x, pos.y, 48f, 48f)
+
+        pos.x ++
+        pos.y ++
+    }
+}

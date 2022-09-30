3cc3759dd40e1d4fc92aebc0ab309570186e7d3d
CherifiMi <cherifi154949@gmail.com>
2022-09-30 04:41:04 +0100

    ball stops at max velocity

--- src/main/kotlin/VectorstBounsingBall.kt
+++ src/main/kotlin/VectorstBounsingBall.kt
@@ -7,6 +7,7 @@ import VectorstBounsingBall.stroke
 import VectorstBounsingBall.width
 import processing.core.PApplet
 import processing.core.PVector
+import kotlin.math.absoluteValue
 
 fun main() {
     VectorstBounsingBall
@@ -21,13 +22,13 @@ object VectorstBounsingBall : PApplet() {
     lateinit var balls: MutableList<Ball>
 
     override fun setup() {
-        background(100f)
+        background(255)
         balls = mutableListOf()
     }
 
     override fun draw() {
         //background(100f)
-        for(b in balls){
+        for (b in balls) {
             b.move()
             b.bounce()
             b.draw()
@@ -38,35 +39,39 @@ object VectorstBounsingBall : PApplet() {
 
     override fun mouseClicked() {
         balls.add(Ball())
-        if (balls.size>5){
+        if (balls.size > 15) {
             balls.removeAt(0)
         }
-        println(balls)
     }
 }
 
 class Ball() {
     var pos = PVector(width / 2f + random(-50f, 50f), height / 2f + random(-50f, 50f))
     var velocity = PVector(2.5f + random(-3f, 3f), -2f + random(-3f, 3f))
-
+    var color = PVector(random(50f, 255f), random(50f, 200f), random(100f, 255f))
     fun move() {
         pos = pos.add(velocity)
     }
 
     fun draw() {
-        fill(random(255f),random(255f),random(255f))
-        stroke(random(255f),random(255f),random(255f))
+        fill(color.x, color.y, color.z)
+        stroke(color.x, color.y, color.z)
         ellipse(pos.x, pos.y, 20f, 20f)
     }
 
     fun bounce() {
+
         if (pos.x >= width.toFloat() || pos.x <= 0f) {
             velocity.x = velocity.x * -1
-            velocity.x += velocity.x * 0.05f
+            if (velocity.x.absoluteValue < 35) {
+                velocity.x += velocity.x * 0.05f
+            }
         }
         if (pos.y >= height.toFloat() || pos.y <= 0f) {
             velocity.y = velocity.y * -1
-            velocity.y += velocity.y * 0.05f
+            if ( velocity.y.absoluteValue < 35){
+                velocity.y += velocity.y * 0.05f
+            }
         }
     }
 }

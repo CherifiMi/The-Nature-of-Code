eb648b13ce5bba8d3c6a6a98cf8a5862378f748e
CherifiMi <cherifi154949@gmail.com>
2022-09-30 04:08:09 +0100

    made singelton object to class

--- src/main/kotlin/VectorstBounsingBall.kt
+++ src/main/kotlin/VectorstBounsingBall.kt
@@ -8,7 +8,7 @@ import VectorstBounsingBall.width
 import processing.core.PApplet
 import processing.core.PVector
 
-fun main(){
+fun main() {
     VectorstBounsingBall
 }
 
@@ -17,22 +17,26 @@ object VectorstBounsingBall : PApplet() {
         this.setSize(400, 400)
         this.runSketch()
     }
+
     lateinit var b: Ball
 
     override fun setup() {
-        b = Ball
+        b = Ball()
+        var a = Ball()
+
+        kotlin.io.println("$b  $a")
     }
 
     override fun draw() {
-            b.move()
-            b.bounce()
-            b.draw()
+        b.move()
+        b.bounce()
+        b.draw()
     }
 }
 
-object Ball{
-    var pos = PVector(width/2f + random(-50f, 50f), height/2f+ random(-50f, 50f))
-    var velocity = PVector(2.5f+ random(-3f,3f), -2f+ random(-3f,3f))
+class Ball() {
+    var pos = PVector(width / 2f + random(-50f, 50f), height / 2f + random(-50f, 50f))
+    var velocity = PVector(2.5f + random(-3f, 3f), -2f + random(-3f, 3f))
 
     fun move() {
         pos = pos.add(velocity)
@@ -46,12 +50,12 @@ object Ball{
     }
 
     fun bounce() {
-        if (pos.x >= width.toFloat() || pos.x <= 0f){
-            velocity.x = velocity.x*-1
+        if (pos.x >= width.toFloat() || pos.x <= 0f) {
+            velocity.x = velocity.x * -1
             velocity.x += velocity.x * 0.05f
         }
-        if (pos.y >= height.toFloat() || pos.y <= 0f){
-            velocity.y = velocity.y*-1
+        if (pos.y >= height.toFloat() || pos.y <= 0f) {
+            velocity.y = velocity.y * -1
             velocity.y += velocity.y * 0.05f
         }
     }

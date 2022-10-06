df15dfaad2dfc952f494d2c465191883f73f20f8
CherifiMi <cherifi154949@gmail.com>
2022-10-07 00:16:55 +0100

    ForceMover init

---
+++ src/main/kotlin/III_force/ForceMover.kt
@@ -0,0 +1,22 @@
+package III_force
+
+import processing.core.PApplet
+
+fun main(){
+    ForceMover
+}
+
+object ForceMover : PApplet() {
+    init {
+        this.setSize(400, 400)
+        this.runSketch()
+    }
+
+    override fun setup() {
+
+    }
+
+    override fun draw() {
+
+    }
+}

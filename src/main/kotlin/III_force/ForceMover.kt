b8dafe49593ce609accfd09562ae0860aa7b6c25
CherifiMi <cherifi154949@gmail.com>
2022-10-07 00:23:38 +0100

    force mover

--- src/main/kotlin/III_force/ForceMover.kt
+++ src/main/kotlin/III_force/ForceMover.kt
@@ -1,6 +1,13 @@
 package III_force
 
+import III_force.ForceMover.ellipse
+import III_force.ForceMover.height
+import III_force.ForceMover.line
+import III_force.ForceMover.mouseX
+import III_force.ForceMover.mouseY
+import III_force.ForceMover.width
 import processing.core.PApplet
+import processing.core.PVector
 
 fun main(){
     ForceMover
@@ -11,12 +18,39 @@ object ForceMover : PApplet() {
         this.setSize(400, 400)
         this.runSketch()
     }
-
-    override fun setup() {
-
-    }
-
     override fun draw() {
-
+        background(255)
+        FM.update()
+        FM.display()
     }
+}
+
+
+object FM{
+    val loc = PVector(width/2f, height/2f)
+    val velocity = PVector()
+    var acceleration = PVector()
+    lateinit var mouse: PVector
+
+    fun update() {
+        mouse= PVector(mouseX.toFloat(), mouseY.toFloat())
+        mouse.sub(loc)
+        mouse.setMag(.5f)
+        acceleration = mouse
+
+        velocity.add(acceleration)
+        loc.add(velocity)
+        velocity.limit(5f)
+    }
+    fun edges() {
+        if (loc.x > width) loc.x = 0f
+        if (loc.x < 0) loc.x = width.toFloat()
+        if (loc.y > height) loc.y = 0f
+        if (loc.y < 0) loc.y = height.toFloat()
+    }
+    fun display() {
+        ellipse(loc.x, loc.y, 50f, 50f)
+        line(loc.x, loc.y, mouseX.toFloat(), mouseY.toFloat())
+    }
+
 }

58ec9851abee89d939c4a658b476382ac155813d
CherifiMi <cherifi154949@gmail.com>
2022-11-24 12:32:52 +0100

    amur

--- src/main/kotlin/IIII_oscillation/AMUR.kt
+++ src/main/kotlin/IIII_oscillation/AMUR.kt
@@ -1,6 +1,9 @@
 package IIII_oscillation
 
 import processing.core.PApplet
+import processing.core.PApplet.constrain
+import processing.core.PConstants.CENTER
+import processing.core.PVector
 
 fun main(){
     AMUR
@@ -12,15 +15,87 @@ object AMUR : PApplet() {
         this.runSketch()
     }
 
-    override fun setup() {
+    var ms = mutableListOf<Mover>()
 
+    override fun setup() {
+        ms.add(Mover(width / 2f, height / 2f, 20f, true))
+
+        for (i in 1..20){
+            ms.add(Mover(random(width.toFloat()), random(height.toFloat()), random(5f, 10f)))
+        }
     }
 
     override fun draw() {
+        background(100f)
 
+        for (m in ms) {
+
+            val gravity = PVector.sub(ms[0].loc, m.loc)
+            val distance = constrain(gravity.mag(), 5f, 25f)
+            val p = (.1f * m.mass * ms[0].mass) / (distance * distance)
+            gravity.normalize()
+            gravity.mult(p)
+
+
+
+            m.applyForce(gravity)
+
+            m.applyMass()
+            m.update()
+            m.display()
+        }
     }
 }
 
-class Mover(){
+class Mover(x: Float, y: Float, m: Float, b: Boolean = false) {
+    val loc = PVector(x, y)
+    val velocity = PVector()
+    var acceleration = PVector()
+
+    var angle = 0f
+    var aVelocity = 0f
+    var aAcceleration = 0f
+
+    val mass = m
+    val b = b
+
+    fun update() {
+        velocity.add(acceleration)
+        loc.add(velocity)
+
+        aAcceleration = acceleration.x.div(10)
+        aVelocity += aAcceleration
+        aVelocity = constrain(aVelocity,-0.1f,0.1f)
+        angle += aVelocity
+
+
+        acceleration.mult(0f)
+    }
+
+    fun display() {
+
+        AMUR.strokeWeight(2f)
+        if (b){
+            AMUR.ellipse(loc.x, loc.y, 2f * mass, 2f * mass)
+        }else{
+            AMUR.stroke(0)
+            AMUR.fill(175f,200f)
+            AMUR.rectMode(CENTER)
+
+            AMUR.pushMatrix()
+            AMUR.rotate(angle)
+            AMUR.rect(loc.x, loc.y,2f * mass, 2f * mass)
+            AMUR.popMatrix()
+        }
+    }
+
+
+    fun applyForce(force: PVector) {
+        acceleration.add(force)
+    }
+
+    fun applyMass() {
+        acceleration.mult(mass)
+    }
 
 }

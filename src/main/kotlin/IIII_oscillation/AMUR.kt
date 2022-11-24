f09ae5e83b6b11bd99f4f46c7999f5549e1a4fc6
CherifiMi <cherifi154949@gmail.com>
2022-11-24 11:18:53 +0100

    amur

---
+++ src/main/kotlin/IIII_oscillation/AMUR.kt
@@ -0,0 +1,26 @@
+package IIII_oscillation
+
+import processing.core.PApplet
+
+fun main(){
+    AMUR
+}
+
+object AMUR : PApplet() {
+    init {
+        this.setSize(400, 400)
+        this.runSketch()
+    }
+
+    override fun setup() {
+
+    }
+
+    override fun draw() {
+
+    }
+}
+
+class Mover(){
+
+}

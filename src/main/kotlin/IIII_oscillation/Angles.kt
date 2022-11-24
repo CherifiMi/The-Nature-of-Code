ec3f4af554992d909c0c3446c170dc1f51f0d74f
CherifiMi <cherifi154949@gmail.com>
2022-11-24 10:28:35 +0100

    spining rect

--- src/main/kotlin/IIII_oscillation/Angles.kt
+++ src/main/kotlin/IIII_oscillation/Angles.kt
@@ -1,2 +1,40 @@
 package IIII_oscillation
 
+import processing.core.PApplet
+
+fun main(){
+    Angles
+}
+// radians = 2 * PI * (degrees / 360)
+
+
+object Angles : PApplet() {
+    init {
+        this.setSize(400, 400)
+        this.runSketch()
+    }
+
+    override fun setup() {
+
+
+    }
+
+    var x = 0f
+
+    override fun draw() {
+        background(100)
+
+        translate(width/2f, height/2f)
+
+
+
+        var angle = radians(x)
+        rotate(angle)
+
+        x++
+
+        rect(-50f, -50f, 100f, 100f)
+
+
+    }
+}
